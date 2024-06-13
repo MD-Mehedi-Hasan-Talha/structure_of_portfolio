121a562f786e020a4e01c3066971c126ea76d90f
unknown <mdmehedihasanbd2006@gmail.com>
2024-06-13 12:40:37 +0600

    This is the first commit.

---
+++ script/app.js
@@ -0,0 +1,35 @@
+const close = document.querySelector("#close");
+const nav = document.querySelector("#nav");
+const iii_dot = document.querySelector("#mobile_nav_3dot");
+const outside_of_navber = document.querySelector(".outside-of-navber");
+const nav_item = document.querySelectorAll(".nav_link");
+const logo = document.querySelector("#logo");
+//
+
+//
+
+//
+
+let closer_arr = [close, outside_of_navber, ...nav_item, logo];
+
+//
+
+//
+
+//
+
+//
+
+iii_dot.addEventListener("click", () => {
+  iii_dot.classList.add("animation");
+  nav.style.transform = "translateX(0)";
+  outside_of_navber.classList.add("clicked");
+});
+
+closer_arr.map((item) => {
+  item.addEventListener("click", (e) => {
+    nav.style.transform = "translateX(-999px)";
+    iii_dot.classList.remove("animation");
+    outside_of_navber.classList.remove("clicked");
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    screens: {
+      md: "950px",
+    },
+    extend: {
+      fontFamily: {
+        // "...": "..."
+      },
+      colors: {
+        primary: "#111827",
+        secondary: "#0891b2",
+      },
+    },
+  },
+  plugins: [],
+};
